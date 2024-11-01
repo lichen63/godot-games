05278e600a6953b3350df337e888ed9166c5dab9
Lichen Liu <liu.lichen63@gmail.com>
2024-11-01 22:14:47 +0800

    remove unused script

--- greedy-snake/scripts/snake_body.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  add_to_group(Configs.GROUP_NAME_SNAKE)
-
-func set_properties(body_index: int) -> void:
-  set_name(Configs.NODE_NAME_SNAKE_BODY_PREFIX + str(body_index))
--- greedy-snake/scripts/walls.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  self.add_to_group(Configs.GROUP_NAME_WALLS)
