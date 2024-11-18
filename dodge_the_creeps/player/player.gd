473751cf573209ab856378f2d6850f0cafab8cb7
Lichen Liu <liu.lichen63@gmail.com>
2024-11-19 07:18:39 +0800

    update player animation

--- dodge_the_creeps/player/player.gd
+++ dodge_the_creeps/player/player.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-# Player actions defined in the Input Map
+#region Player constants
 const PLAYER_ACTION_LEFT: String = "player_action_left"
 const PLAYER_ACTION_RIGHT: String = "player_action_right"
 const PLAYER_ACTION_UP: String = "player_action_up"
 const PLAYER_ACTION_DOWN: String = "player_action_down"
+const PLAYER_ANIMATION_RIGHT_OR_LEFT: String = "right_or_left"
+const PLAYER_ANIMATION_UP_OR_DOWN: String = "up_or_down"
+#endregion
 
-# Player constants
 const PLAYER_MOVE_SPEED: int = 200
 
 @onready var animated_sprite_: AnimatedSprite2D = $Area2D/AnimatedSprite2D
@@ -22,8 +24,32 @@ func _physics_process(delta: float) -> void:
     )
     if direction.length() > 0:
         direction = direction.normalized()
-        self.position += direction * PLAYER_MOVE_SPEED * delta
-    animated_sprite_.play("left")
+        var new_position: Vector2 = self.position + direction * PLAYER_MOVE_SPEED * delta
+        self.position = clamp_to_screen(new_position)
+    update_animation(direction)
+
+func clamp_to_screen(pos: Vector2) -> Vector2:
+    # 获取当前窗口大小
+    var viewport_rect = get_viewport().get_visible_rect()
+    return Vector2(
+        clamp(pos.x, viewport_rect.position.x, viewport_rect.position.x + viewport_rect.size.x),
+        clamp(pos.y, viewport_rect.position.y, viewport_rect.position.y + viewport_rect.size.y)
+    )
 
 func update_animation(direction: Vector2) -> void:
-    pass
+    if direction.length() <= 0:
+        animated_sprite_.stop()
+        return
+    var flip_h: bool = false
+    var flip_v: bool = false
+    var dir_animation: String = ""
+    if direction.x > 0 or direction.x < 0:
+        dir_animation = PLAYER_ANIMATION_RIGHT_OR_LEFT
+        flip_h = direction.x < 0
+    elif direction.y > 0 or direction.y < 0:
+        dir_animation = PLAYER_ANIMATION_UP_OR_DOWN
+        flip_v = direction.y > 0
+    animated_sprite_.animation = dir_animation
+    animated_sprite_.flip_h = flip_h
+    animated_sprite_.flip_v = flip_v
+    animated_sprite_.play()
