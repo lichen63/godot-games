953f93346ee3ba3294d292fddb3fd06068ca4bb0
Lichen Liu <liu.lichen63@gmail.com>
2024-11-18 21:57:30 +0800

    rename art to img, add scene game and player, add previews folder

---
+++ dodge_the_creeps/player/player.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Player actions defined in the Input Map
+const PLAYER_ACTION_LEFT: String = "player_action_left"
+const PLAYER_ACTION_RIGHT: String = "player_action_right"
+const PLAYER_ACTION_UP: String = "player_action_up"
+const PLAYER_ACTION_DOWN: String = "player_action_down"
+
+# Player constants
+const PLAYER_MOVE_SPEED: int = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+    var direction: Vector2 = Vector2(
+        Input.get_action_strength(PLAYER_ACTION_RIGHT) - Input.get_action_strength(PLAYER_ACTION_LEFT),
+        Input.get_action_strength(PLAYER_ACTION_DOWN) - Input.get_action_strength(PLAYER_ACTION_UP)
+    )
+    if direction.length() > 0:
+        direction = direction.normalized()
+        self.position += direction * PLAYER_MOVE_SPEED * delta
