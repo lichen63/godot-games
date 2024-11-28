557e15156f751c7033bdae767f1c2500dc5a4d45
Lichen Liu <liu.lichen63@gmail.com>
2024-11-28 21:59:27 +0800

    update enemy node, suspect curve2d has bug

--- dodge_the_creeps/enemy/enemy.gd
+++ dodge_the_creeps/enemy/enemy.gd
@@ -18,17 +18,18 @@ func _ready():
     var end_pos: Vector2 = get_random_pos_on_edge(get_opposite_edge(start_edge))
     self.position = start_pos
     var curve_node: Curve2D = $Path2D.curve
-    # curve_node.clear_points()
+    curve_node.clear_points()
     curve_node.add_point(Vector2.ZERO)
-    curve_node.add_point(Vector2(450, 1000) - start_pos)
     curve_node.add_point(end_pos - start_pos)
 
     var follow_node: PathFollow2D = $Path2D/PathFollow2D
     follow_node.loop = false
 
+    # 打印日志
     print("start_pos: ", start_pos)
     print("end_pos: ", end_pos)
-    print("curve_node points: ", curve_node.get_point_position(0), ", ", curve_node.get_point_position(1), ",", curve_node.get_point_position(2))
+    for i in range(curve_node.get_point_count()):
+        print("curve_node point ", i, ": ", curve_node.get_point_position(i))
 
 func _physics_process(delta: float) -> void:
     var follow_node = $Path2D/PathFollow2D
@@ -40,13 +41,13 @@ func _physics_process(delta: float) -> void:
 func get_random_pos_on_edge(edge_dir: EdgeDir) -> Vector2:
     match edge_dir:
         EdgeDir.TOP:
-            return Vector2(randf() * screen_size_.x, -50)
+            return Vector2(randf() * screen_size_.x, 0)
         EdgeDir.BOTTOM:
-            return Vector2(randf() * screen_size_.x, screen_size_.y + 50)
+            return Vector2(randf() * screen_size_.x, screen_size_.y)
         EdgeDir.LEFT:
-            return Vector2(-100, randf() * screen_size_.y)
+            return Vector2(0, randf() * screen_size_.y)
         EdgeDir.RIGHT:
-            return Vector2(screen_size_.x + 100, randf() * screen_size_.y)
+            return Vector2(screen_size_.x, randf() * screen_size_.y)
     return Vector2.ZERO
 
 func get_opposite_edge(edge_dir: EdgeDir) -> EdgeDir:
