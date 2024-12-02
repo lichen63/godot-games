18702e9ab07843262c0bae6cb37bf4ddb417a5d0
Lichen Liu <liu.lichen63@gmail.com>
2024-12-02 21:11:53 +0800

    update game node from node2d to node

--- dodge_the_creeps/game/game.gd
+++ dodge_the_creeps/game/game.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 const ENEMY_SCENE: PackedScene = preload("res://enemy/enemy.tscn")
 const AUDIO_BACKGROUND: AudioStream = preload("res://assets/audio/background.ogg")
