e22b4a2786a3c0491d17dbca2b1c66b6971eec54
Lichen Liu <liu.lichen63@gmail.com>
2024-12-08 18:36:27 +0800

    complete chatper 3: tilemap

--- adventure/world/world.gd
+++ adventure/world/world.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-@onready var tile_map_layer_ : TileMapLayer = $TileMapLayer
+@onready var tile_map_layer_1_ : TileMapLayer = $TileMapLayer1
 @onready var camera_2d_: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
-    var used: Rect2i = tile_map_layer_.get_used_rect()
-    var tile_size: Vector2i = tile_map_layer_.tile_set.tile_size
+    var used: Rect2i = tile_map_layer_1_.get_used_rect().grow(-1)
+    var tile_size: Vector2i = tile_map_layer_1_.tile_set.tile_size
     
     camera_2d_.limit_top = used.position.y * tile_size.y
     camera_2d_.limit_right = used.end.x * tile_size.x
