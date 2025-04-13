0696d48fd4905317b8fd48072ad099eb1d058d9e
Lichen <liu.lichen63@gmail.com>
2025-04-13 20:11:57 +0800

    add profile scene

--- immortal-cultivation-travel/scenes/player/player.gd
+++ immortal-cultivation-travel/scenes/player/player.gd
@@ -4,17 +4,11 @@ extends CharacterBody2D
 const MOVE_SPEED: float = 800.0
 const ACCELERATION_FLOOR: float = MOVE_SPEED / 0.2
 const ACCELERATION_AIR: float = MOVE_SPEED / 0.1
-const JUMP_VELOCITY: float = -900.0
+const JUMP_VELOCITY: float = -800.0
 const ANIMATION_IDLE: String = "idle"
 const ANIMATION_RUN: String = "run"
 const ANIMATION_ATTACK: String = "attack"
 const ANIMATION_DEAD: String = "dead"
-const INPUT_ACTION_LEFT: String = "player_left"
-const INPUT_ACTION_RIGHT: String = "player_right"
-const INPUT_ACTION_UP: String = "player_up"
-const INPUT_ACTION_DOWN: String = "player_down"
-const INPUT_ACTION_INTERACT: String = "player_interact"
-const INPUT_PLAYER_JUMP: String = "player_jump"
 
 var gravity: float = ProjectSettings.get("physics/2d/default_gravity") as float
 var interacting_with: Array[Interactable] = []
@@ -26,18 +20,18 @@ func _ready() -> void:
   self.animated_sprite.play()
 
 func _unhandled_input(event: InputEvent) -> void:
-  if event.is_action_pressed(INPUT_ACTION_INTERACT) and not self.interacting_with.is_empty():
+  if event.is_action_pressed("player_interact") and not self.interacting_with.is_empty():
     self.interacting_with.back().interact()
 
 func _physics_process(delta: float) -> void:
   # set velocity of x
-  var direction: float = Input.get_axis(INPUT_ACTION_LEFT, INPUT_ACTION_RIGHT)
+  var direction: float = Input.get_axis("player_left", "player_right")
   var acceleration: float = ACCELERATION_FLOOR if self.is_on_floor() else ACCELERATION_AIR
   self.velocity.x = move_toward(self.velocity.x, direction * MOVE_SPEED, acceleration * delta)
 
   # set velocity of y
   self.velocity.y += gravity * delta
-  if self.is_on_floor() and Input.is_action_pressed(INPUT_PLAYER_JUMP):
+  if self.is_on_floor() and Input.is_action_pressed("player_jump"):
     self.velocity.y = JUMP_VELOCITY
 
   # set animation
