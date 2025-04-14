1976537bfa5946eb5dc31ae127be4dc574e5103b
Lichen <liu.lichen63@gmail.com>
2025-04-14 22:12:28 +0800

    support switch language

--- immortal-cultivation-travel/scenes/startup/startup.gd
+++ immortal-cultivation-travel/scenes/startup/startup.gd
@@ -8,3 +8,12 @@ func _ready() -> void:
 
 func _on_start_pressed() -> void:
   self.get_tree().change_scene_to_file("res://scenes/home/home.tscn")
+
+func _on_language_item_selected(index: int) -> void:
+  match(index):
+    0:
+      TranslationServer.set_locale("zh")
+    1:
+      TranslationServer.set_locale("en")
+    _:
+      print("unsupported language: %d" % index)
